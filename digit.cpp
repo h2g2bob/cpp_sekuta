3506c1e1724af416e265d58b69171ad45e0fdd36
David Batley <git@dbatley.com>
2025-12-27 16:52:14 +0000

    Move method definitions to cpp file

--- digit.cpp
+++ digit.cpp
@@ -5,3 +5,26 @@ using namespace digit;
 Digit digit::digitFromSingleValue(int singleValue){
   return Digit {1 << singleValue};
 }
+
+int Digit::countOptions() const {
+  int options = 0;
+  for (int temp = data; temp; temp = temp >> 1) {
+    if (temp & 0x1) {
+      options ++;
+    }
+  }
+  return options;
+}
+
+int Digit::getData() const {
+  return data;
+}
+
+int Digit::getValue() const {
+  for (int i = MIN; i <= MAX; i++) {
+    if (data == 1 << i) {
+      return i;
+    }
+  }
+  return 0;
+}
--- digit.h
+++ digit.h
@@ -11,28 +11,9 @@ namespace digit {
     Digit(): data{ALL_OPTIONS} {};
     Digit(int data_): data {data_} {};
 
-    int countOptions() const {
-      int options = 0;
-      for (int temp = data; temp; temp = temp >> 1) {
-        if (temp & 0x1) {
-          options ++;
-        }
-      }
-      return options;
-    }
-
-    int getData() const {
-      return data;
-    }
-
-    int getValue() const {
-      for (int i = MIN; i <= MAX; i++) {
-        if (data == 1 << i) {
-          return i;
-        }
-      }
-      return 0;
-    }
+    int countOptions() const;
+    int getData() const;
+    int getValue() const;
   };
 
 
