04bcee3f6d3621a20aaf3d987353a220db6bdbfa
David Batley <git@dbatley.com>
2025-12-30 18:28:57 +0000

    state: multi-line description

--- state.cpp
+++ state.cpp
@@ -5,11 +5,12 @@ using namespace state;
 
 std::string State::describe() const {
   std::ostringstream out;
-  out << "State";
+  out << "State:\n";
   for (int y = 0; y < GRIDSZ; y++) {
     for (int x = 0; x < GRIDSZ; x++) {
-      out << " " << y << x << "=" << digits[y][x].describe();
+      out << " " << digits[y][x].describe();
     }
+    out << "\n";
   }
   return out.str();
 }
