0903f5a453167f9dcbc869dc08fe1de79b68a526
David Batley <git@dbatley.com>
2026-01-01 12:34:21 +0000

    Use *this instead of copying the values by hand

--- state.cpp
+++ state.cpp
@@ -22,14 +22,7 @@ State State::setValue(const int y, const int x, const Digit newDigit) const {
   assert(y >= 0 && y < GRIDSZ);
   assert(x >= 0 && x < GRIDSZ);
 
-  State out {};
-
-  // the dumb way to copy all the values:
-  for (int iy = 0; iy < GRIDSZ; iy++) {
-    for (int ix = 0; ix < GRIDSZ; ix++) {
-      out.digits[iy][ix] = digits[iy][ix];
-    }
-  }
+  State out = *this;
   out.digits[y][x] = newDigit;
 
   return out;
